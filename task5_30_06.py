0d02d38863bd5bd05742115a5e902ddfc245e2ab
LCIMS2 <bykova-elena772@bk.ru>
2025-06-30 15:31:50 +0300

    Unpack

---
+++ task5_30_06.py
@@ -0,0 +1,13 @@
+# Кортеж (tuple, immutable)
+
+# channels = ['red', 'green', 'blue']
+#
+# r, g, b = channels # распаковка
+#
+# print(g)
+
+channels = [128, 200, 155]
+
+a, b, c = channels
+
+print(a,b,c)

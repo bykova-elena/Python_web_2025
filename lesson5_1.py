7059564c8a18c3d3fa977c8d38d9cf35deebc338
LCIMS2 <bykova-elena772@bk.ru>
2025-07-01 11:15:00 +0300

    Comprehension task

---
+++ lesson5_1.py
@@ -0,0 +1,22 @@
+# Списочные выражения (list comprehension)
+
+text = 'Списочные выражения применяются для эффективности кода'
+
+
+
+
+
+
+
+
+
+
+# произведение i и j
+
+# print([i * j for i in range(3) for j in range (3)])
+
+# n = '100 200 300 400 500 600 700 800 900'
+# approved = ['500', '800']
+# a = [int(i) for i in n.split() if i in approved]
+# какие-то действия со списком a
+# print(a)

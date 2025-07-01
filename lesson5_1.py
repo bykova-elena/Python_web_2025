2388c1cb32697fedb0e4b4b9ce3d7b5780ebabcc
LCIMS2 <bykova-elena772@bk.ru>
2025-07-01 12:25:54 +0300

    Comprehension task

--- lesson5_1.py
+++ lesson5_1.py
@@ -1,9 +1,61 @@
+# Вложенные списки
+from lesson_30_06 import count
+
+matrix = [[1] * N for _ in range (N)
+print(matrix)
+# обход 2-мерного списка (матрицы)
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        matrix[row] [col] = count
+        count += 1
+
+print(matrix)
+
+# matrix = [
+#    [1, 2, 3],
+#    [4, 5, 6],
+#    [7, 8, 9],
+#]
+
+matrix = [[1] * 3 for _ in range (3)]
+print(matrix)
+
+# обход 2-мерного списка (матрицы)
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        print(matrix[row][col])
+print(matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Списочные выражения (list comprehension)
 
-text = 'Списочные выражения применяются для эффективности кода'
+# text = 'Списочные выражения применяются для эффективности кода'
 
+# res = [a for a in text.split() if (text.index(a)+1) % 3 == 0]
+# res = [a for a in text.split()[2::3]]
 
-
+# print(res)
 
 
 
